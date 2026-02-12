9f280090e151087dd64e1431cadbe967d0c09634
Sandeep Kumar Yadav <sandeepkyadav017@gmail.com>
2026-02-12 13:43:47 +0530

    okf

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://skill-exchange-server-u91b.vercel.app',
         changeOrigin: true,
       },
     },
